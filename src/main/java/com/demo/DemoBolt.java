15364baedb66c7cea71d2e5419025e812ff155ef
刘万杰 <crab_liu@kingdee.com>
2016-12-26 12:19:27 +0800

    update

--- src/main/java/com/demo/DemoBolt.java
+++ src/main/java/com/demo/DemoBolt.java
@@ -1,5 +1,8 @@
 package com.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import backtype.storm.topology.BasicOutputCollector;
 import backtype.storm.topology.OutputFieldsDeclarer;
 import backtype.storm.topology.base.BaseBasicBolt;
@@ -13,12 +16,12 @@ public class DemoBolt extends BaseBasicBolt{
 	 * 
 	 */
 	private static final long serialVersionUID = 2046633838564394830L;
-
+	Logger logger = LoggerFactory.getLogger(DemoBolt.class);
 	@Override
 	public void execute(Tuple arg0, BasicOutputCollector arg1) {
 		String msg = arg0.getString(0);
 		int value = arg0.getIntegerByField("rand");
-		System.err.println(String.format("%s******** rand:%s", msg,value));
+		logger.info("content:{},rand:{}",msg,value);
 	}
 
 	@Override
